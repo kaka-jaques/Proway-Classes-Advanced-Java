14f3a5a38f21a52114d7999dcf944984b4e7ebfc
kaka-jaques <kjfakhouri@gmail.com>
2022-05-31 23:11:50 -0300

    AULA JAVA AVANÇADA

---
+++ JAVA-AVANCADO/src/br/com/entra21/javaavancado/kalil/Main.java
@@ -0,0 +1,102 @@
+package br.com.entra21.javaavancado.kalil;
+
+import java.util.Scanner;
+
+import br.com.entra21.javaavancado.kalil.enums.PersonagemJogo;
+
+public class Main {
+
+	static Scanner input = new Scanner(System.in);
+
+	//Sintaxe do ENUM
+	private enum DiasSemana{
+		SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO
+	}
+	
+	public static void main(String[] args) {
+
+		byte option;
+
+		do {
+
+			System.out.println(exibirMenu());
+			option = input.nextByte();
+
+			switch (option) {
+			case 0:
+
+				System.out.println("Saindo");
+
+				break;
+			case 1:
+
+				aprenderWrapper();
+
+				break;
+			case 2:
+
+				aprenderENUM();
+
+				break;
+
+			default:
+				System.out.println("Opção Inválida!");
+				break;
+			}
+
+		} while (option != 0);
+
+	}
+
+	private static String exibirMenu() {
+
+		String menu = "Vamos aprender sobre conceitos avançados em JAVA!";
+		menu += "\n0 - Sair";
+		menu += "\n1- Wrapper";
+		menu += "\n2 - ENUM";
+		menu += "\nEscolha uma das Opções";
+
+		return menu;
+	}
+
+	private static void aprenderWrapper() {
+		
+		//sintade do Wrapper
+		
+		char letra = Character.valueOf('k');
+		boolean verdade = Boolean.parseBoolean("true");
+		boolean verdade2 = Boolean.parseBoolean("TRUE");
+		boolean verdade3 = Boolean.parseBoolean("tRuE");
+		boolean falso = Boolean.parseBoolean("t");
+		
+		byte inteoro1 = Byte.parseByte("127");
+		
+		short inteiro2 = Short.parseShort("128");
+		
+		int inteiro3 = Integer.parseInt("10000");
+		
+		long inteiro4 = Long.parseLong("12839138");
+
+	}
+	
+	public static void aprenderENUM() {
+		
+		//ENUM são variaveis constantes (final) mas sem necessita de uma declaração inicial
+		//Ela já é inicializada na declaração e o próprio nome dela server com valor
+		
+		System.out.println("Testando "+DiasSemana.TERCA);
+		boolean hoje = DiasSemana.DOMINGO == DiasSemana.SEXTA;
+		System.out.println(hoje ?"sim":"não");
+		
+		//Listando os dias como se fossem vetores
+		for(int dia = 0; dia < DiasSemana.values().length; dia++) {
+			System.out.println((dia++) +" - "+ DiasSemana.values()[dia]);
+		}
+		
+		System.out.println("Gosto desse tipo de personagem: "+PersonagemJogo.ANAO);
+		System.out.println("Gosto desse tipo de personagem: "+PersonagemJogo.ANAO+" / HP = "+PersonagemJogo.ANAO.getHP());
+		System.out.println("Gosto desse tipo de personagem: "+PersonagemJogo.ANAO+" / CLASSE = "+PersonagemJogo.ANAO.getDESCRIPTION());
+		
+	}
+
+}
---
+++ JAVA-AVANCADO/src/br/com/entra21/javaavancado/kalil/enums/PersonagemJogo.java
@@ -0,0 +1,23 @@
+package br.com.entra21.javaavancado.kalil.enums;
+
+public enum PersonagemJogo {
+
+	BARBARO(1000, "TANK"), ANAO(100, "FERREIRO"), DRUIDA(50, "SUPORTE"), SARCEDOTE(10, "CURA");
+	
+	private final int HP;
+	private final String DESCRIPTION;
+	
+	private PersonagemJogo(int vida, String descricao) {
+		this.HP = vida;
+		this.DESCRIPTION = descricao;
+	}
+
+	public int getHP() {
+		return HP;
+	}
+
+	public String getDESCRIPTION() {
+		return DESCRIPTION;
+	}
+	
+}
